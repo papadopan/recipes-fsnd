8bbe3c2fb1cf1c11c895a7bc5632283b5e160220
Antonios Papadopoulos <antoniospapadopoulos@Antonioss-MacBook-Pro.local>
2020-04-26 18:30:25 +0200

    backend: schema init

---
+++ recipes/app.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+@app.route("/")
+def hello():
+    return "Hello"
