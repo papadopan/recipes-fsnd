fd1dfc5691359266d0b30b5d9a41eba48f86315d
Antonios Papadopoulos <antoniospapadopoulos@Antonioss-MacBook-Pro.local>
2020-04-26 20:05:49 +0200

    backend: database config

--- recipes/app.py
+++ recipes/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
---
+++ recipes/config.py
@@ -0,0 +1,2 @@
+SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://antoniospapadopoulos@localhost:5432/recipes"
+SQLALCHEMY_TRACK_MODIFICATIONS = "FALSE"
