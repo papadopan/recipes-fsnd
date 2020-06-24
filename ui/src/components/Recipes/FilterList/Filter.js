2ffc714a3eaf05d6cb843ea8a6339645ef0c2b9f
Antonios Papadopoulos <antoniospapadopoulos@Antonioss-MacBook-Pro.local>
2020-06-25 00:22:33 +0200

    frontend: add categories for filtering

--- ui/src/components/Recipes/FilterList/Filter.js
+++ ui/src/components/Recipes/FilterList/Filter.js
@@ -1,27 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Wrapper = styled.div`
   background: red;
-  height: 80px;
-  min-width: 300px;
+  height: 50px;
+  min-width: 150px;
   margin-right: 1em;
   border-radius: 40px;
   background-image: linear-gradient(
       to right,
-      rgba(0, 0, 0, 0.3),
-      rgba(0, 0, 0, 0.3)
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0.5)
     ),
-    url("https://images.unsplash.com/photo-1549736624-81a2ca809ad7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
+    url("https://images.unsplash.com/photo-1464965911861-746a04b4bca6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
   background-position: top;
   background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+
+  ${({ link }) =>
+    link &&
+    css`
+      background-image: linear-gradient(
+          to right,
+          rgba(0, 0, 0, 0.5),
+          rgba(0, 0, 0, 0.5)
+        ),
+        url(${link});
+    `}
 `;
 
-const Filter = (props) => {
-  return <Wrapper>antoios</Wrapper>;
+const Filter = ({ item }) => {
+  return <Wrapper link={item.link}>{item.name}</Wrapper>;
 };
 
-Filter.propTypes = {};
+Filter.propTypes = {
+  item: PropTypes.object,
+};
 
 export default Filter;
--- ui/src/components/Recipes/FilterList/FilterList.js
+++ ui/src/components/Recipes/FilterList/FilterList.js
@@ -4,16 +4,38 @@ import Filter from "./Filter";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
+  margin: 1em 0;
   display: flex;
 `;
 
-const l = [{ name: "mexican" }, { name: "italian" }];
+const categories = [
+  {
+    name: "Vegan",
+    link:
+      "https://images.unsplash.com/photo-1549736624-81a2ca809ad7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "Vegetarian",
+    link:
+      "https://images.unsplash.com/photo-1591189863430-ab87e120f312?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "italian",
+    link:
+      "https://images.unsplash.com/photo-1516685018646-549198525c1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "burger",
+    link:
+      "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  },
+];
 
 const FilterList = (props) => {
   return (
     <Wrapper>
-      {l.map((item) => (
-        <Filter />
+      {categories.map((item) => (
+        <Filter item={item} />
       ))}
     </Wrapper>
   );
